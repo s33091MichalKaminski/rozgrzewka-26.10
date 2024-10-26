556fd3583f8b532c9185c03ded0e2856d634a842
s33091MichalKaminski <michal.kaminski232@gmail.com>
2024-10-26 14:35:07 +0200

    Create rozgrzewka_26_10.cpp

---
+++ rozgrzewka_26_10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int kawalki;
+   int pizza;
+   int ilosc;
+   int uczestnicy;
+   int gosp;
+   cout<<"podaj liczbe gości"<<endl;
+   cin>>uczestnicy;
+   cout<<"podaj liczbe pizz"<<endl;
+   cin>>pizza;
+   kawalki=pizza*8;
+   ilosc = kawalki/uczestnicy;
+   gosp=kawalki-(ilosc*uczestnicy);
+   cout<<"kazdy gosc dostanie "<<ilosc<<" kawalki pizzy, a gospodarz dostanie "<<gosp<<endl;
+
+
+    return 0;
+}
